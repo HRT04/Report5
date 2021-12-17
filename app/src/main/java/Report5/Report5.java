b59bad6aceea3f1546ff20ee3f880f9a68a3d628
Haruto Adaniya <e215707@ie.u-ryukyu.ac.jp>
2021-12-17 20:53:06 +0900

    Step2

--- app/src/main/java/Report5/Report5.java
+++ app/src/main/java/Report5/Report5.java
@@ -2,9 +2,17 @@ package Report5;
 
 public class Report5 {
     public static void main(String[] args){
+        try{
         String str = null;
         int s = str.length();
         System.out.println(s);
+        }catch(NullPointerException e){
+            System.out.println("--------------------------------------");
+            System.out.println("例外「NullPointerException」発生");
+            System.out.println(e.getMessage());
+            System.out.println("--------------------------------------");
+        }
+        
     }
     
 }
