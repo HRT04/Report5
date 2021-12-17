b83389c59c5de756af76c6fa5ee36a4e8373ba3b
Haruto Adaniya <e215707@ie.u-ryukyu.ac.jp>
2021-12-17 18:36:19 +0900

    Step1

---
+++ app/src/main/java/Report5/Report5.java
@@ -0,0 +1,10 @@
+package Report5;
+
+public class Report5 {
+    public static void main(String[] args){
+        String str = null;
+        int s = str.length();
+        System.out.println(s);
+    }
+    
+}
